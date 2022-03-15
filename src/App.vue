9555b314159bfc6172f4ab00897e89d9049cc0d2
ribamarf01 <ribamar.f01@gmail.com>
2022-03-14 21:16:49 -0300

    initial config

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full h-screen flex justify-center dark:bg-black dark:text-white">
+    <div>Left nav</div>
+    <div>Content</div>
+    <div>Suggestions</div>
+  </div>
+</template>
+
+<style>
+
+</style>
